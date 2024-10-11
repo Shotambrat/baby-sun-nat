648173520e6fb359e64596eda6056e72e42aa5ff
Faizullin Abdukhakim <dudumikah@gmail.com>
2024-10-11 15:00:31 +0500

    feat: update: footer mobile socials

--- components/shared/Footer/Footer.tsx
+++ components/shared/Footer/Footer.tsx
@@ -26,39 +26,39 @@ export const Footer = ({ className }: Props) => {
               width={100}
               height={100}
               alt="Instagrm Icon in Footer"
-              className="w-10"
+              className="w-10 max-mdx:w-12"
             />
             <Image
               src={"/svg/footer/telegram.svg"}
               width={100}
               height={100}
               alt="Instagrm Icon in Footer"
-              className="w-10"
+              className="w-10 max-mdx:w-12"
             />
             <Image
               src={"/svg/footer/facebook.svg"}
               width={100}
               height={100}
               alt="Instagrm Icon in Footer"
-              className="w-10"
+              className="w-10 max-mdx:w-12"
             />
             <Image
               src={"/svg/footer/youtube.svg"}
               width={100}
               height={100}
               alt="Instagrm Icon in Footer"
-              className="w-10"
+              className="w-10 max-mdx:w-12"
             />
           </div>
         </div>
         <div className="flex flex-col gap-2">
           <h3 className="text-2xl max-mdx:text-4xl font-semibold">Адрес</h3>
-          <p className="text-sm leading-5 font-medium w-full max-w-[250px]">
+          <p className="text-sm leading-5 max-mdx:text-xl font-medium w-full max-mdx:max-w-full max-w-[250px]">
             г.Ташкент, ул Такая-то, проезд такой-то, дом такой-то
           </p>
           <Link
             href={"/contacts"}
-            className="text-[#009FE3] flex gap-4 font-bold items-center"
+            className="text-[#009FE3] max-mdx:text-xl flex gap-4 font-bold items-center"
           >
             Построить маршрут
             <Image
@@ -71,39 +71,39 @@ export const Footer = ({ className }: Props) => {
           </Link>
         </div>
         <div className="flex flex-col gap-2">
-          <h3 className="text-2xl font-semibold">Связаться с нами</h3>
+          <h3 className="text-2xl max-mdx:text-4xl font-semibold">Связаться с нами</h3>
           <a
             href="tel:+998909999999"
-            className="text-sm leading-5 font-medium w-full max-w-[250px]"
+            className="text-sm max-mdx:text-xl leading-5 font-medium w-full max-w-[250px]"
           >
             +998 (90) 999-99-99
           </a>
           <a
             href="tel:+998909999999"
-            className="text-sm leading-5 font-medium w-full max-w-[250px]"
+            className="text-sm max-mdx:text-xl leading-5 font-medium w-full max-w-[250px]"
           >
             +998 (90) 123-45-67
           </a>
           <a
             href="milto:name@domain.com"
-            className="text-sm leading-5 font-medium w-full max-w-[250px]"
+            className="text-sm max-mdx:text-xl leading-5 font-medium w-full max-w-[250px]"
           >
             name@domain.com
           </a>
         </div>
         <div className="flex flex-col gap-2">
-          <h3 className="text-2xl font-semibold">График работы</h3>
-          <p className="text-sm leading-5 font-medium w-full max-w-[250px]">
+          <h3 className="text-2xl max-mdx:text-4xl font-semibold">График работы</h3>
+          <p className="text-sm max-mdx:text-xl leading-5 font-medium w-full max-w-[250px]">
             8:00 - 17:00 пн-пт
           </p>
-          <p className="text-sm leading-5 font-medium w-full max-w-[250px]">
+          <p className="text-sm max-mdx:text-xl leading-5 font-medium w-full max-w-[250px]">
             8:00 - 15:00 сб
           </p>
         </div>
       </div>
       <hr />
       <div className="flex w-full justify-between max-w-[1500px] px-4 mx-auto py-4">
-        <div className="flex gap-12 max-lgx: flex-wrap">
+        <div className="flex gap-12 max-mdx:gap-4 max-lgx: flex-wrap">
           <p className="text-[#879EBC]">
           2024 © BabySun. Все права защищены
           </p>
