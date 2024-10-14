ba6ca82fa19bc5789c83051934572ec28e1cf638
Faizullin Abdukhakim <dudumikah@gmail.com>
2024-10-14 15:29:59 +0500

    feat: create: Courses About

--- components/shared/Courses/Courses.tsx
+++ components/shared/Courses/Courses.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { cn } from '@lib/utils';
 import { Banner } from './Banner/Banner';
+import { About } from './About/About';
 
 interface Props {
   className?: string;
@@ -10,6 +11,7 @@ export const Courses = ({ className }: Props) => {
   return (
     <div className={cn(className)}>
         <Banner />
+        <About />
     </div>
   );
 };
