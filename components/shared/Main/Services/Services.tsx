9fd6302a3fbfeb3cb9e34065c7dc232dfd7fc2b3
musya <dudumikah@gmail.com>
2024-11-04 11:24:36 +0500

    feat: update: ourservices section

--- components/shared/Main/Services/Services.tsx
+++ components/shared/Main/Services/Services.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { cn } from "@lib/utils";
 import { Link } from "@/i18n/routing";
 import Image from "next/image";
-import { Button } from "@/components/ui";
+// import { Button } from "@/components/ui";
 
 interface Props {
   className?: string;
